964ea673d3c3abe2b7a2d454d80db7e8adceb54f
TemuulenKhuchit <temuulenkhuchit01@gmail.com>
2025-11-12 00:24:12 -0600

    Day 30

--- day30_errors_exceptions_and_json_data/main.py
+++ day30_errors_exceptions_and_json_data/main.py
@@ -1,1 +1,15 @@
-print("Exception handling")
+try:
+    file = open("data.json")
+except FileNotFoundError:
+    with open("data.json", "w") as file:
+        file.write('{"key": "value"}')
+except KeyError as error_message:
+    print(f"Key Error: {error_message}")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+
+
+# Upgraded Day 29 project with error handling and JSON data
